69fb723764e6722d78d34799ddfc7a2113c97b82
Lapp <lapp-coder@mail.ru>
2022-03-03 14:58:07 +0300

    project: implemented

---
+++ README.md
@@ -0,0 +1,39 @@
+# Docsel 
+[![Go](https://img.shields.io/badge/go-1.17-blue)](https://golang.org/doc/go1.17) [![Release](https://img.shields.io/badge/release-1.0.0-success)](https://github.com/Lapp-coder/docsel/releases)
+
+Docsel is a utility that allows you to run the services you choose based on the docker-compose file.
+
+## Example
+![docsel example](https://github.com/Lapp-coder/docsel/blob/resourses/docsel-example.gif)
+
+## Installation 
+1. Clone the repository
+```shell
+$ git clone https://github.com/Lapp-coder/docsel
+```
+2. Go to the directory of the utility 
+```shell
+$ cd docsel
+```
+3. Follow the installation steps for your OS
+* MacOS on M1
+```shell
+$ chmod +x build/bin/docsel-darwin_arm64 && \
+make setup-macos_m1
+```
+* MacOS on Intel
+```shell
+$ chmod +x build/bin/docsel-darwin_amd64 && \
+make setup-macos_intel
+```
+
+* Linux
+```shell
+$ chmod +x build/bin/docsel-linux_amd64 && \
+make setup-linux
+```
+
+* Windows
+```shell
+$ setup-windows.bat
+```
---
+++ internal/app/command.go
@@ -0,0 +1,232 @@
+package app
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"github.com/eiannone/keyboard"
+
+	"os"
+
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
+)
+
+const helpMessage = `Select the service you want by pressing Enter when the arrow on the left points to it. 
+You can move the arrow to the keys j (down), k (up) or the arrows ↓ ↑. 
+Press the Ctrl + D key combination to start your selected services.
+`
+
+const (
+	NameRootCmd      = "docsel"
+	ShortDescRootCmd = "Docsel is a utility that allows you to run your selected services in Docker based on the docker-compose file"
+)
+
+const (
+	NameRunCmd      = "run"
+	ShortDescRunCmd = "Launches the service selection panel"
+
+	FlagPath             = "path"
+	ShorthandFlagPath    = "p"
+	DescFlagPath         = "Path to docker-compose file"
+	DefaultFlagPathValue = "docker-compose.yaml"
+
+	FlagDetach             = "detach"
+	ShorthandFlagDetach    = "d"
+	DescFlagDetach         = "Running selected services in the background"
+	DefaultFlagDetachValue = false
+
+	FlagRemove             = "rm"
+	DescFlagRemove         = "Removes stopped service containers. Ignored in detached mode."
+	DefaultFlagRemoveValue = false
+
+	FlagSave             = "save"
+	ShorthandFlagSave    = "s"
+	DescFlagSave         = "Saves the docker-compose file generated with the selected services"
+	DefaultFlagSaveValue = false
+)
+
+var RootCmd = &cobra.Command{
+	Use:   NameRootCmd,
+	Short: ShortDescRootCmd,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := cmd.Help(); err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+const (
+	firstNameGeneratedFile = "docker-compose.yaml"
+	lastNameGeneratedFile  = "docsel.yaml"
+)
+
+var errEmptyServices = errors.New("empty services in docker-compose file")
+
+var RunCmd = &cobra.Command{
+	Use:   NameRunCmd,
+	Short: ShortDescRunCmd,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cleanConsole()
+
+		filepath := cmd.Flag(FlagPath).Value.String()
+		fileContent, err := os.ReadFile(filepath)
+		if err != nil {
+			return err
+		}
+
+		var dc DockerCompose
+		if err := yaml.Unmarshal(fileContent, &dc); err != nil {
+			return err
+		}
+
+		if len(dc.Services) == 0 {
+			return errEmptyServices
+		}
+
+		recordNumber := 1
+		fmt.Println(helpMessage, generateDashboard(dc, false, recordNumber))
+
+		var filename string
+		switch filepath {
+		case "docker-compose.yaml", "docker-compose.yml", "compose.yaml", "compose.yml":
+			filename = lastNameGeneratedFile
+		default:
+			filename = firstNameGeneratedFile
+		}
+
+	Label:
+		file, err := os.Create(filename)
+		if err != nil {
+			if errors.Is(err, os.ErrExist) {
+				if err = os.Remove(filename); err != nil {
+					return err
+				}
+				goto Label
+			}
+
+			return err
+		}
+		defer func() {
+			if cmd.Flag(FlagSave).Value.String() == "false" {
+				if err = file.Close(); err != nil {
+					log.Fatalln(err)
+				}
+				if err = os.Remove(filename); err != nil {
+					log.Fatalln(err)
+				}
+			} else {
+				fileInfo, err := file.Stat()
+				if err != nil {
+					log.Fatalln(err)
+				}
+
+				if fileInfo.Size() > 0 {
+					if err = file.Close(); err != nil {
+						log.Fatalln(err)
+					}
+
+					if filename == lastNameGeneratedFile {
+						return
+					} else if filename == firstNameGeneratedFile {
+						if err = os.Rename(filename, lastNameGeneratedFile); err != nil {
+							log.Fatalln(err)
+						}
+						return
+					}
+				}
+
+				if err = os.Remove(filename); err != nil {
+					log.Fatalln(err)
+				}
+			}
+		}()
+
+		for {
+			char, key, err := keyboard.GetSingleKey()
+			if err != nil {
+				return err
+			}
+
+			switch key {
+			case keyboard.KeyCtrlD:
+				if len(selectedServices) > 0 {
+					for service := range dc.Services {
+						if _, ok := selectedServices[service]; !ok {
+							delete(dc.Services, service)
+						}
+					}
+
+					b, err := yaml.Marshal(dc)
+					if err != nil {
+						return err
+					}
+
+					if _, err = file.WriteString(string(b)); err != nil {
+						return err
+					}
+
+					if cmd.Flag(FlagDetach).Value.String() == "false" {
+						dockerCmd := exec.Command("docker-compose", "-f", filename, "up")
+						dockerCmd.Stderr = os.Stderr
+						dockerCmd.Stdout = os.Stdout
+						if err := dockerCmd.Start(); err != nil {
+							return err
+						}
+
+						quit := make(chan os.Signal, 1)
+						signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+						<-quit
+
+						if cmd.Flag(FlagRemove).Value.String() == "false" {
+							dockerCmd = exec.Command("docker-compose", "stop")
+						} else {
+							dockerCmd = exec.Command("docker-compose", "rm", "--stop", "-f")
+						}
+
+						dockerCmd.Stderr = os.Stderr
+						dockerCmd.Stdout = os.Stdout
+
+						return dockerCmd.Run()
+					}
+
+					dockerCmd := exec.Command("docker-compose", "-f", filename, "up", "-d")
+					dockerCmd.Stderr = os.Stderr
+					dockerCmd.Stdout = os.Stdout
+					return dockerCmd.Run()
+				}
+
+				fmt.Println("none of the services has been selected")
+
+				return nil
+			case keyboard.KeyCtrlC:
+				return nil
+			case keyboard.KeyEnter:
+				cleanConsole()
+				fmt.Println(helpMessage, generateDashboard(dc, true, recordNumber))
+			default:
+				if key == keyboard.KeyArrowUp || char == 'k' {
+					if recordNumber > 1 {
+						recordNumber--
+
+						cleanConsole()
+						fmt.Println(helpMessage, generateDashboard(dc, false, recordNumber))
+					}
+				} else if key == keyboard.KeyArrowDown || char == 'j' {
+					if recordNumber < len(services) {
+						recordNumber++
+
+						cleanConsole()
+						fmt.Println(helpMessage, generateDashboard(dc, false, recordNumber))
+					}
+				}
+			}
+		}
+	},
+}

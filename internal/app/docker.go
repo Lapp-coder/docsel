46aa74269e3e919c3db3f19a686e3943c6163dd7
Lapp <lapp-coder@mail.ru>
2022-03-06 19:48:37 +0300

    project: add gif example

--- internal/app/docker.go
+++
@@ -1,8 +0,0 @@
-package app
-
-type DockerCompose struct {
-	Version  string                            `yaml:"version"`
-	Services map[string]map[string]interface{} `yaml:"services"`
-	Networks map[string]interface{}            `yaml:"networks,omitempty"`
-	Volumes  map[string]interface{}            `yaml:"volumes,omitempty"`
-}
--- setup-windows.bat
+++
@@ -1,3 +0,0 @@
-mkdir "C:\Program Files\docsel"
-copy build\bin\docsel-windows_amd64.exe "C:\Program Files\docsel\docsel.exe"
-setx PATH "C:\Program Files\docsel;%PATH%"
